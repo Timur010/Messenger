429ec8495f9b33f033c17ffe2f041ed6b626c178
Timur010 <duakar010@gmail.com>
2023-07-10 13:41:07 +0300

    commit

---
+++ Messenger/Core/Authentication/View/LiginView.swift
@@ -0,0 +1,18 @@
+//
+//  LiginView.swift
+//  Messenger
+//
+//  Created by Timur Kadiev on 10.07.2023.
+//
+
+import SwiftUI
+
+struct LiginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LiginView()
+}
